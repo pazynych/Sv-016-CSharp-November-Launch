82ea37d99d51521932a9e206dd3ad5a209e901f6
pazynych <pazynych.1@gmail.com>
2013-12-15 22:14:53 +0200

    Add return to Send function_Test commit

---
+++ trunk/sv016cs-webdd/app.dal/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* The components of this layer implement the system core functionality and encapsulate all 
+ * the relevant business logic. Basically, this layer usually 
+ * contains classes which implement the domain logic within their methods. 
+ * This layer also defined a Unit of Work contract within the Core Layer so 
+ * it will be able to comply with the PI principle.
+ */
+
+namespace app.core
+{
+    public class Class1
+    {
+        /// <summary>
+        /// comment
+        /// </summary>
+        public int MyProperty { get; set; }
+        // test comment
+        //cometa run!
+    }
+
+    public class TerClass
+    {
+        /// <summary>
+        /// comment
+        /// </summary>
+        int Send ()
+        {
+            int a = 1 + 1;
+            return a;
+        }
+
+        public int MyProperty33 { get; set; }
+        // test comment
+    }
+
+
+}
