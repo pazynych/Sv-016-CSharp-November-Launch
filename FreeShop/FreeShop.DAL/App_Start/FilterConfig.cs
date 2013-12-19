22827d4e3fd35287abed076dcc508fade30ffac4
arexol <arexol@gmail.com>
2013-12-19 16:47:09 +0200

    added web API project for data access layer

---
+++ FreeShop/FreeShop.DAL/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace FreeShop.DAL
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
