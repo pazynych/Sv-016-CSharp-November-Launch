5d0355635f5647d1c540c327fa2421aa293dc21f
arexol <arexol@gmail.com>
2013-12-19 19:41:23 +0200

    Database re-creation approach changed - now it re-creates on each run

--- FreeShop/FreeShop.DAL/DAL/OrderInitializer.cs
+++ FreeShop/FreeShop.DAL/DAL/OrderInitializer.cs
@@ -9,15 +9,16 @@ using FreeShop.DAL.Models;
 
 namespace FreeShop.DAL.DAL
 {
-    public class OrderInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<OrderContext>
+    //public class OrderInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<OrderContext>
+    public class OrderInitializer : System.Data.Entity.DropCreateDatabaseAlways<OrderContext>
     {
 
         protected override void Seed(OrderContext context)
         {
             var users = new List<User>
             {
-                new User{ UserID = 1 ,NickName="Arexol"         ,FirstName="Alex", LastName="Serkin",Email = "E@A.ru",Region = "Sevastopol",Role = "Young Padavan "},
-                new User{ UserID = 2 ,NickName="bsesie71"       ,FirstName=" x"  , LastName="S"     ,Email = "E@A.ru",Region = "Sevastopol",Role = "Young Padavan "},
+                new User{ UserID = 1 ,NickName="Arexol"         ,FirstName="Alex", LastName="Serkin",Email = "E@A.ru",Region = "Sevastopol",Role = "YoungPadavan"},
+                new User{ UserID = 2 ,NickName="bsesie71"       ,FirstName="x"  , LastName="S"     ,Email = "E@A.ru",Region = "Sevastopol",Role = "YoungPadavan"},
                 new User{ UserID = 3 ,NickName="intsiren"       ,FirstName="buuu", LastName="www"   ,Email = "E@A.ru",Region = "Sevastopol",Role = "Young Padavan "},
                 new User{ UserID = 4 ,NickName="igor.aka.ky7m"  ,FirstName="Igor", LastName="II    ",Email = "E@A.ru",Region = "Sevastopol",Role = "Obi van kanobi"},
                 new User{ UserID = 5 ,NickName="SoftServe Ita"  ,FirstName="AAA ", LastName="BB    ",Email = "E@A.ru",Region = "Sevastopol",Role = "Dark Vaider   "}
