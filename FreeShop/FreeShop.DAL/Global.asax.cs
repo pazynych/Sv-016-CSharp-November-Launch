413547561e3d0e7efdb149a5e4339ee84f45655e
arexol <arexol@gmail.com>
2013-12-20 16:21:18 +0200

    Added log4net logging library - working succesfully

--- FreeShop/FreeShop.DAL/Global.asax.cs
+++ FreeShop/FreeShop.DAL/Global.asax.cs
@@ -8,20 +8,27 @@ using System.Web.Optimization;
 using System.Web.Routing;
 using FreeShop.Common;
 
+using log4net;
+using log4net.Config;
+
 namespace FreeShop.DAL
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
- 
+        public static readonly ILog log = LogManager.GetLogger(typeof(WebApiApplication)); 
         
         protected void Application_Start()
         {
+            
+            log4net.Config.XmlConfigurator.Configure();
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
+
+            log.Info("Logger test");
             // ConsoleManager.InitializeConsoleManager();
             // ConsoleManager.EventLogerInit();
             // 
